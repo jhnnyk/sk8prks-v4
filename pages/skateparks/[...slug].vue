b06117bf8d4a75b9ea78f30fbf34f86635de5520
John Knotts <jhnnyk@gmail.com>
2025-03-28 14:30:34 -0600

    add map links to skatepark page

--- pages/skateparks/[...slug].vue
+++ pages/skateparks/[...slug].vue
@@ -11,7 +11,18 @@ const { data: page } = await useAsyncData(route.path, () => {
     <h1 class="text-2xl">{{ page.title }}</h1>
     <p class="mb-4">
       {{ page.street }}<br />
-      {{ page.city }}, {{ page.state }} {{ page.zipcode }}
+      {{ page.city }}, {{ page.state }} {{ page.zipcode }}<br />
+      <a
+        class="text-pink-500"
+        :href="`https://www.google.com/maps/search/?api=1&query=${page.latitude}%2C${page.longitude}`"
+        >Google Maps</a
+      >
+      |
+      <a
+        class="text-pink-500"
+        :href="`http://maps.apple.com/?ll=${page.latitude},${page.longitude}&q=${page.title}`"
+        >Apple Maps</a
+      >
     </p>
     <p class="mb-4">{{ page.description }}</p>
     <ContentRenderer :value="page" />
